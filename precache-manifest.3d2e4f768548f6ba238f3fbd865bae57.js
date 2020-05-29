87804d62bbc1bde45cd0a2120cfe66d96d79724c
lucky4030 <lucky@.github.com>
2020-05-29 17:28:53 +0530

    Updates

---
+++ precache-manifest.3d2e4f768548f6ba238f3fbd865bae57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9837861bc5b67d521b8eea4a5f052fa7",
+    "url": "/deploy-app/index.html"
+  },
+  {
+    "revision": "31732b325f0f8455c437",
+    "url": "/deploy-app/static/css/main.f8be79fe.chunk.css"
+  },
+  {
+    "revision": "625b3e8913f3e0ffade2",
+    "url": "/deploy-app/static/js/2.1baedafd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-app/static/js/2.1baedafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31732b325f0f8455c437",
+    "url": "/deploy-app/static/js/main.eda1695f.chunk.js"
+  },
+  {
+    "revision": "bd059b3db13ea8caeaec",
+    "url": "/deploy-app/static/js/runtime-main.26c046bf.js"
+  }
+]);
